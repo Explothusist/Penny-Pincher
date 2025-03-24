2c56e5ee5d3e5c63face2cb1bda637f6b0a69992
one-some <onesomegit@outlook.com>
2025-03-24 09:26:43 -0500

    FRMT!

--- src/routes/import/+page.server.ts
+++ src/routes/import/+page.server.ts
@@ -1,6 +1,12 @@
-import { Income, Expense, Balance, Category, adaptiveFromCSV } from '$lib/db.server';
-import { redirect } from '@sveltejs/kit';
-import Database from 'better-sqlite3';
+import {
+    Income,
+    Expense,
+    Balance,
+    Category,
+    adaptiveFromCSV,
+} from "$lib/db.server";
+import { redirect } from "@sveltejs/kit";
+import Database from "better-sqlite3";
 const db = new Database("db/main.db", {});
 db.pragma("journal_mode = WAL");
 
@@ -10,15 +16,14 @@ export function load({ cookies, url }) {
     load functions will not run when changing pages unless something referenced in the function, such as url, changes. Maybe could be replaced with invalidateAll()? 
     It must be url.pathname too, not just url.  */
 
-    const categories = Category.get_all().map(x => x.toJSON());
+    const categories = Category.get_all().map((x) => x.toJSON());
 
     return {
-        categories: categories
+        categories: categories,
     };
-};
+}
 
 export const actions = {
-
     importCSV: async ({ cookies, request, url }) => {
         /* HACK: According to https://kit.svelte.dev/docs/load#rerunning-load-functions-when-do-load-functions-rerun
         load functions will not run when changing pages unless something referenced in the function, such as url,
@@ -35,35 +40,52 @@ export const actions = {
         const contents = await csvFile.text();
 
         const currentBalance = Balance.current();
+
+        // -1 as sentinel to keep previous
         const overrideCategory = importCategory !== -1;
 
         // Will be corresponding constructor, or adaptiveFromCSV if unapplicable.
-        const csvConstructor = {
-            1: Income.fromCSV,
-            2: Expense.fromCSV,
-        }[importType] || adaptiveFromCSV;
+        const csvConstructor =
+            {
+                1: Income.fromCSV,
+                2: Expense.fromCSV,
+            }[importType] || adaptiveFromCSV;
 
         // Transactions will be objects of their respective types, `Income` or `Expense`.
-        const transactions = contents.split("\n").map(csvConstructor).filter(x => x);
+        const transactions = contents
+            .split("\n")
+            .map(csvConstructor)
+            .filter((x) => x);
 
         if (overrideCategory) {
-            transactions.forEach((transaction) => transaction.category = importCategory);
+            transactions.forEach(
+                (transaction) => (transaction.category = importCategory)
+            );
         }
 
         for (const transaction of transactions) {
             // SECURITY: Ensure `table` is of known values to avoid SQL injection
             const table = transaction instanceof Income ? "income" : "expense";
-            db.prepare(`INSERT INTO ${table} (amount, date, source) VALUES (?, ?, ?)`)
-                .run(transaction.amountUsd, transaction.date, transaction.category);
+            db.prepare(
+                `INSERT INTO ${table} (amount, date, source) VALUES (?, ?, ?)`
+            ).run(
+                transaction.amountUsd,
+                transaction.date,
+                transaction.category
+            );
 
             // Subtract if an expense
-            currentBalance.amountUsd += transaction.amountUsd * (transaction instanceof Income ? 1 : -1);
+            currentBalance.amountUsd +=
+                transaction.amountUsd *
+                (transaction instanceof Income ? 1 : -1);
         }
 
-        db.prepare("UPDATE balance SET amount = ? WHERE id = ?").run(currentBalance.amountUsd, 1);
+        db.prepare("UPDATE balance SET amount = ? WHERE id = ?").run(
+            currentBalance.amountUsd,
+            1
+        );
 
         // Redirect to home
         throw redirect(303, "/");
-    }
-
+    },
 };
